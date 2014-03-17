5d23a58134198f5c5588e7b2b988d4674b6c0dc0
unknown <shourav9884@gmail.com>
2014-03-17 12:24:08 +0600

    User class added

---
+++ src/com/example/pinboxproject/entity/User.java
@@ -0,0 +1,43 @@
+package com.example.pinboxproject.entity;
+
+public class User {
+	int id;
+	String userName,email;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
