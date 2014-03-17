8712cee5b4cb8b931e74d92eb022a2edf06c3ac4
unknown <shourav9884@gmail.com>
2014-03-17 12:27:16 +0600

    Settings added

---
+++ src/com/example/pinboxproject/entity/Settings.java
@@ -0,0 +1,6 @@
+package com.example.pinboxproject.entity;
+
+public class Settings {
+	public static User loggedUser;
+	public static String BASE_URL="http://pinbox-nath.rhcloud.com/index.php/user/";
+}
